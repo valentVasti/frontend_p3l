52c012c63dd671a237b5bf747831f7cfc55ba494
Your Name <you@example.com>
2023-06-08 23:28:41 +0700

    tes lagi 2

--- src/components/loginPage.vue
+++ src/components/loginPage.vue
@@ -22,7 +22,7 @@
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-md-6 text-center mb-5">
-          <h2 class="heading-section">Go-Fit</h2>
+          <h2 class="heading-section">Go-Fit Test Netlify</h2>
         </div>
       </div>
       <div class="row justify-content-center">
